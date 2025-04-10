f5f15553f425022088d348cb27aff47f2888e137
peterakerblom <43443378+peterakerblom@users.noreply.github.com>
2025-04-10 21:15:39 +0200

    commit

--- scripts/cultivation_bed.gd
+++ scripts/cultivation_bed.gd
@@ -5,8 +5,17 @@ extends Area2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var rect_shape: RectangleShape2D = collision_shape.shape
 
+@export var flower_scene = preload("res://scenes/flowers/red_tulip.tscn")
+@export var flower_spawn_interval: float = 8.0
+
+var timer : Timer
+
 func _ready():
 	update_sprite_scale()
+	timer = $Timer
+	timer.wait_time = flower_spawn_interval
+	timer.one_shot = false  # Så att timern fortsätter återkomma
+	timer.start()  # Starta timern direkt
 
 #func _process(delta):
 	#if Engine.is_editor_hint():
@@ -29,3 +38,15 @@ func update_sprite_scale():
 		# Centrera spriten genom att justera dess position
 		# Anpassa till mitten av kollisionen
 		#sprite.position = Vector2(-target_size.x / 2, -target_size.y / 2)
+
+
+func _on_timer_timeout() -> void:
+	# Hämta Area2D's storlek
+	var area_size = rect_shape.size  # Om din Area2D är en rektangel
+	# Generera en slumpmässig position inom området
+	var random_position = Vector2(randf_range(0, area_size.x), randf_range(0, area_size.y))
+	
+	# Spawna scenen på den slumpmässiga positionen
+	var instance = flower_scene.instantiate()
+	instance.position = random_position
+	get_parent().add_child(instance)  # Lägg till den spawna scenen som ett barn till föräldern (eller välj en annan nod)
--- scripts/flower.gd
+++ scripts/flower.gd
@@ -1,11 +1,35 @@
 extends Area2D
 
+@export var timer_duration : float = 3.0
+var timer : Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	timer = $Timer  # Se till att timern är som barn till Area2D
+	timer.wait_time = timer_duration
+	timer.one_shot = true  # Om du vill att timern ska fortsätta eller inte
+	timer.stop()  # Stoppa timern från att börja direktdy.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print(body.name + " enters area")
+		timer.start()
+	
+	
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print(body.name + " leaves area")
+		timer.stop()
+
+
+func _on_timer_timeout() -> void:
+	print("Timern har löpt ut!")
+	queue_free()
