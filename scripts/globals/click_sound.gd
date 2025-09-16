11a9e9894db3fdda4db4cf11629c3fbd00cef013
peterakerblom <43443378+peterakerblom@users.noreply.github.com>
2025-09-16 21:48:19 +0200

    refactor

--- scripts/globals/click_sound.gd
+++ scripts/globals/click_sound.gd
@@ -3,18 +3,12 @@ extends Node
 var click_sound: AudioStreamPlayer
 
 func _ready():
-	#get_tree().node_added.connect(_on_node_added)
 	# Skapa AudioStreamPlayer via kod
 	click_sound = AudioStreamPlayer.new()
 	add_child(click_sound)
 	
 	# Ladda ljudfilen från resursen
 	click_sound.stream = load("res://assets/sound/click_sound.wav")
-	#click_sound.volume_db = 0  # Justera volym om du vill
 	
 func play_click() -> void:
 	click_sound.play()
-	#
-#func _on_node_added(node):
-	#if node is Button:
-		#node.pressed.connect(play_click)
